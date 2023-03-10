d8e95b48faea3643424e60f7eb936559204acb68
Guilherme Medina <gui.castro1906@gmail.com>
2023-03-10 11:06:57 -0300

    modified:   .vscode/settings.json modified:   jogodaadivinhacao.cpp modified:   output/jogodaadivinhacao.exe

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -10,14 +10,22 @@ int main(){
     const int NUMERO_SECRETO = 42;
 
     bool nao_acertou = true;
+    int tentativas = 0;
+
+    double pontos = 1000.0;
 
     while (nao_acertou)
     {
+        tentativas += 1;
+        cout << "\nTentativa " << tentativas << ":" << endl;
         int chute;
         cout << "Qual eh seu chute? ";
         cin >> chute;
+        
+        double pontosPerdidos = abs(chute - NUMERO_SECRETO)/2.0;
+        pontos = pontos - pontosPerdidos;
 
-        cout << "O valor do seu chute eh: " << chute << endl;
+        // cout << "O valor do seu chute eh: " << chute << endl;
 
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
@@ -37,5 +45,10 @@ int main(){
         }
     }
     
+    cout << "\n\nVoce precisou de " << tentativas << " tentativas para encontrar o numero." << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuacao foi de " << pontos << " pontos." << endl;
+    cout << "\n\n==== Fim de Jogo ====" << endl;
 
 } 
